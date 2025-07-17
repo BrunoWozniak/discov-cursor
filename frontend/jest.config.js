86765f1036442559d5b1861da971a7f4a381e797
BrunoWozniak <bruno.wozniak@gmail.com>
2025-07-17 23:57:10 +0200

    Fix: enable @testing-library/jest-dom matchers in Jest setup

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js']
 }; 

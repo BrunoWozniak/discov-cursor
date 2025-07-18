c2be641877b69348dfb2c82372e02f61ff3290c5
BrunoWozniak <bruno.wozniak@gmail.com>
2025-07-18 13:10:47 +0200

    Ensure test results and coverage directories exist and are configured for all test runners

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   e2e: {
     baseUrl: 'http://frontend:3000',
-    supportFile: false
+    supportFile: false,
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: './test-results/frontend-e2e-junit.xml',
+      toConsole: true
+    }
   }
 }; 

4e16234cb2d17269f966bffd7e64647136aed30f
BrunoWozniak <bruno.wozniak@gmail.com>
2025-07-21 00:12:02 +0200

    Cleanup: remove unused components, fix backend API test results output

--- frontend/src/components/Select.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export function Select(props) { return <select {...props} /> }
-export function SelectTrigger(props) { return <div {...props} /> }
-export function SelectContent(props) { return <div {...props} /> }
-export function SelectGroup(props) { return <div {...props} /> }
-export function SelectLabel(props) { return <label {...props} /> }
-export function SelectItem(props) { return <option {...props} /> }
-export function SelectValue(props) { return <span {...props} /> } 
